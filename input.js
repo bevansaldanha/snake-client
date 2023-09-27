5e847929751aa191aaa91d8f13ab69627951b098
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-27 15:21:31 -0400

    Added logic to send data to the server and added movement logic

--- input.js
+++ input.js
@@ -1,15 +1,38 @@
-const setupInput = function() {
+let connection;
+let curInterval;  
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  connection = conn;
+  
   return stdin;
 };
 
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+
+  if (key === 'w') {
+    clearInterval(curInterval);
+    curInterval = setInterval(() => connection.write("Move: up"), 10) 
+  }
+  if (key === 'a') {
+    clearInterval(curInterval);
+    curInterval = setInterval(() => connection.write("Move: left"), 10) 
+  }
+
+  if (key === 's') {
+    clearInterval(curInterval);
+    curInterval = setInterval(() => connection.write("Move: down"), 10)  
+  }
+
+  if (key === 'd') {
+    clearInterval(curInterval);
+    curInterval = setInterval(() => connection.write("Move: right"), 10) 
   }
 };
 module.exports = setupInput;
